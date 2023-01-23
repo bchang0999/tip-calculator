b432e16dea6653f1d2fc7ec90ac1a66553774237
Brandon Chang <bchang0999@gmail.com>
2023-01-23 16:24:06 -0500

    cleaning up code

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-
-const sliders = document.querySelectorAll("input[type='range']");
-console.log(sliders); 
-sliders.forEach(function(slider){
-  slider.addEventListener("input", calculateTip)
+const sliders = document.querySelectorAll('input[type="range"]');
+sliders.forEach(function (slider) {
+    slider.addEventListener("input", calculateTip)
 })
 
-const billInput = document.getElementById("bill");
-billInput.addEventListener("change",calculateTip);
-console.log(billInput);
+const billInput = document.getElementById('bill');
+billInput.addEventListener("change", calculateTip)
 
-function calculateTip(){
-  let bill = parseFloat(billInput.value);
-  let tipPercent = document.getElementById("tip").value;
-  let noOfPeople = document.getElementById("no-of-people").value;
+function calculateTip() {
+    let bill = parseFloat(billInput.value)
+    let tipPercent = document.getElementById('tip').value;
+    let noOfPeople = document.getElementById('no-of-people').value;
 
-  billInput.value = bill.toFixed(2)
+    billInput.value = bill.toFixed(2);
 
-  let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2));
-  let total = parseFloat((bill + totalTip).toFixed(2));
-  let tipPerPerson = (totalTip / noOfPeople).toFixed(2);
-  let totalPerPerson = (total / noOfPeople).toFixed(2);
+    let totalTip = parseFloat((bill * (tipPercent / 100)).toFixed(2));
+    console.log("The Tip is", totalTip, "Dollars")
+    let total = parseFloat((bill + totalTip).toFixed(2))
+    console.log("The total is", total, "Dollars")
 
-  console.log(noOfPeople ,totalTip, total, tipPerPerson, totalPerPerson);
+    let tipPerPerson = parseFloat(totalTip / noOfPeople).toFixed(2);
+    console.log("The tip per person is", tipPerPerson, "Dollars")
 
-  document.getElementById("tip-amount").textContent = `\$ ${totalTip}`;
-  document.getElementById("total-amount").textContent = `\$ ${total}`;
+    let totalPerPerson = parseFloat(total / noOfPeople).toFixed(2);
+    console.log("The total per person is", totalPerPerson, "Dollars")
 
-  document.getElementById("tip-percent").textContent =`${tipPercent}%`;
-  document.getElementById("split-num").textContent = noOfPeople
 
-  document.getElementById("tip-per-person").textContent = `\$ ${tipPerPerson}`;
-  document.getElementById("total-per-person").textContent = `\$ ${totalPerPerson}`;
+    document.getElementById('split-num').textContent = noOfPeople
+    document.getElementById('tip-percent').textContent = tipPercent
+
+    console.log("the bill amount is", bill, "the tip pecentage is", tipPercent, "there are ", noOfPeople, "people")
+
+    document.getElementById("tip-amount").textContent = `\$ ${totalTip}`;
+    document.getElementById("total-amount").textContent = `\$ ${total}`;
+
+    document.getElementById("tip-percent").textContent = `${tipPercent}%`;
+    document.getElementById("split-num").textContent = noOfPeople
+
+    document.getElementById("tip-per-person").textContent = `\$ ${tipPerPerson}`;
+    document.getElementById("total-per-person").textContent = `\$ ${totalPerPerson}`;
+
 }
